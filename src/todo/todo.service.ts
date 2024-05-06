a04e761028fe809a389a8831f5933bd121fa8680
wgutierrez <adolfoguti001@outlook.com>
2024-05-05 20:32:53 -0600

    basic CRUD with nest.js

---
+++ src/todo/todo.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
+
+@Injectable()
+export class TodoService {
+  private todos: Todo[] = [
+    {id: 1, description: 'Piedra del Alma', done: false},
+    {id: 2, description: 'Piedra del Tiempo', done: false},
+    {id: 3, description: 'Piedra del Espacio', done: false}
+  ]
+  create(createTodoDto: CreateTodoDto): Todo {
+    const todo = new Todo();
+    todo.id = Math.max(...this.todos.map(todo => todo.id), 0) + 1;
+    todo.description = createTodoDto.description;
+    this.todos.push(todo)
+    return todo
+  }
+
+  findAll(): Todo[] {
+    return this.todos;
+  }
+
+  findOne(id: number) {
+    const todo = this.todos.find(todo => todo.id === id);
+    if (!todo) throw new NotFoundException(`This ID #${id} does not exist`)
+    return todo;
+  }
+
+  update(id: number, updateTodoDto: UpdateTodoDto): Todo {
+    const { done, description } = updateTodoDto;
+    const todo = this.findOne(id);
+
+    if (done !== undefined) todo.done = done;
+    if (description !== undefined) todo.description = description;
+
+    this.todos = this.todos.map( dbToodo => {
+      if (dbToodo.id === id) return todo;
+      return dbToodo;
+    })
+    return todo;
+  }
+
+  remove(id: number) {
+    this.findOne(id);
+    this.todos = this.todos.filter( todo => todo.id !== id);
+  }
+}
